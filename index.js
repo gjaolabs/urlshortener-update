086a8d7577abb4c4b98de2d9536af7a8bc364058
unknown <nodus8tollens>
2023-06-11 19:19:31 +0200

    Everything works

--- index.js
+++ index.js
@@ -1,70 +1,121 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(console.log("DB CONNECTION SUCCESFUL"))
+  .catch((err) => console.error(err));
+
+app.get("/db-health", (req, res) => {
+  res.json({ status: mongoose.connection.readyState });
+});
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
 // When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
 // If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
 
-const originalUrls = [];
-const shortUrls = [];
+const URLSchema = mongoose.Schema(
+  {
+    longURL: String,
+    shortURL: String,
+    issuedDate: Date,
+  },
+  { versionKey: false }
+);
 
-app.post("/api/shorturl", (req, res) => {
+const URL = mongoose.model("URL", URLSchema);
+
+function shorten() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let shortURL = "";
+  for (i = 0; i < 8; i++) {
+    shortURL += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return shortURL;
+}
+
+async function removeOldEntries() {
+  const thirtyDaysAgo = Date.now() - 2592000000;
+  try {
+    const deleteObj = await URL.deleteMany({
+      issuedDate: { $lte: thirtyDaysAgo },
+    });
+    console.log(`Number of documents deleted: ${deleteObj.deletedCount}`);
+  } catch (err) {
+    console.error("Error: ", err);
+  }
+}
+
+//Post URL (check if its already in the collection; if YES return and extend timer), convert and save to MongoDB, return JSON
+
+app.post("/api/shorturl", async (req, res) => {
+  removeOldEntries();
   const url = req.body.url;
 
-  const foundIndex = originalUrls.indexOf(url);
-
-  if(!url.includes("https://") && !url.includes("http://")){
-    return res.json({ error: 'invalid url' })
+  if (!url.includes("https://") && !url.includes("http://")) {
+    return res.json({ error: "invalid url" });
   }
-
-  if(foundIndex < 0) {
-    originalUrls.push(url)
-    shortUrls.push(shortUrls.length)
-
-    return res.json({
-      original_url: url,
-      short_url: shortUrls.length - 1
-    })
+  try {
+    const checkObject = await URL.findOne({ longURL: url });
+    if (checkObject?.shortURL) {
+      checkObject.issuedDate = Date.now();
+      await checkObject.save();
+      res.json({ shortURL: checkObject.shortURL });
+    } else {
+      const newObject = new URL({
+        longURL: url,
+        shortURL: shorten(),
+        issuedDate: Date.now(),
+      });
+      await newObject.save();
+      res.json({ shortURL: newObject.shortURL });
+    }
+  } catch (err) {
+    res.send("Error: " + err);
   }
+});
 
-  return res.json({
-    original_url: url,
-    short_url: shortUrls[foundIndex]
-  })
+// add /api/get-link endpoint to read 'http://service-domain/{publicId}', find mongo entry by checking the id, and then respond the real link
 
-})
-
-app.get("/api/shorturl/:shorturl", (req, res) => {
-  const shorturl = parseInt(req.params.shorturl);
-  const foundIndex = shortUrls.indexOf(shorturl)
-
-  if(foundIndex < 0) {
-    return res.json({"error": "No short URL found for the given unput"})
+app.get("/:shorturl", async (req, res) => {
+  const shorturl = req.params.shorturl;
+  try {
+    const checkObject = await URL.findOne({ shortURL: shorturl });
+    res.json({ "Long URL": checkObject.longURL });
+  } catch (err) {
+    res.send(err);
   }
-  
-  res.redirect(originalUrls[foundIndex])
-})
+});
 
-app.listen(port, "0.0.0.0", function() {
+// add another endpoint that instead od responding string, will execute 302 redirect. Map it to 'service-domain/l/publicId'
+
+app.get("/l/:publicId", async (req, res) => {
+  const shorturl = req.params.publicId;
+  try {
+    const checkObject = await URL.findOne({ shortURL: shorturl });
+    res.redirect(checkObject.longURL);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.listen(port, "0.0.0.0", function () {
   console.log(`Listening on port ${port}`);
 });
