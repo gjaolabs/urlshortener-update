89c5978453ebe57c94b0deec9edc2cf87bf250ca
mugren <igorkivor@gmail.com>
2023-06-18 15:10:13 +0200

    syntax

--- mongo-connector.js
+++ mongo-connector.js
@@ -4,7 +4,7 @@ const { MONGO_DB_URL } = require('./config')
 function connect() {
   mongoose
     .connect(MONGO_DB_URL)
-    .then(console.log("DB CONNECTION SUCCESFUL"))
+    .then(() => console.log("DB CONNECTION SUCCESFUL"))
     .catch((err) => console.error(err));
 }
 
