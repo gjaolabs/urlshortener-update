2293071f9cc52b6e237f188956cd509c6304e141
mugren <igorkivor@gmail.com>
2023-06-18 15:01:35 +0200

    move mongo setup to separate modules

---
+++ mongo-models.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const URLSchema = mongoose.Schema(
+  {
+    longURL: String,
+    shortURL: String,
+    issuedDate: Date,
+  },
+  { versionKey: false }
+);
+
+const URL = mongoose.model("URL", URLSchema);
+
+module.exports = {
+  URL: URL
+}
