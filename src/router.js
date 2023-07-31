ad2edce79b5df77450c2ffe7e6f4b9ed836dccb4
mugren <igorkivor@gmail.com>
2023-07-31 19:23:09 +0200

    update routes definitions

--- src/router.js
+++ src/router.js
@@ -2,7 +2,9 @@ const { Router } = require("express");
 const controller = require("./controller");
 
 const router = Router();
-router.post("/api/shorturl", controller.postEntry);
-router.get("/api/get-url/:shorturl", controller.getEntry);
-router.get("/api/redirect/:shorturl", controller.getRedirect);
+router.post("/api/shorturls", controller.postEntry);
+router.get("/api/shorturls/:shorturl", controller.getEntry);
+// Does not consider this endpoint as 'headless' api, but a link that would be
+// opened in a browser as {domain}/open/123456
+router.get("/open/:shorturl", controller.getRedirect);
 module.exports = router;
