f5e471a29768e17fbc80e2c40584227cb049deeb
nodus8tollens <mladenovskipredrag@gmail.com>
2023-09-11 08:24:18 +0200

    Postgres init script. Keeps connecting to default db (postgres) instead of url_shortener db. Maybe it's a Linux thing. Will look into it

--- src/postgres/db.js
+++ src/postgres/db.js
@@ -12,4 +12,33 @@ const pool = new Pool({
   port: process.env.PORT,
 });
 
+const { Client } = require('pg');
+
+//IMPORTANT: tried to connect to dedicated url_shortener db via .env file - keeps connecting to default postgres db
+
+const client = new Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+console.log(process.env.USER, process.env.HOST, process.env.DATABASE, process.env.PASSWORD, process.env.PORT);
+
+
+client.connect();
+client.query(`
+  CREATE TABLE IF NOT EXISTS url_shortener (
+    id SERIAL PRIMARY KEY,
+    longurl TEXT NOT NULL,
+    shorturl TEXT NOT NULL,
+    issueddate TIMESTAMP NOT NULL DEFAULT NOW()
+)`, (err, res) => {
+  console.log(err ? err.stack : 'Table created successfully');
+  client.end();
+});
+
+
+
 module.exports = pool;
