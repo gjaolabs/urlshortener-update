f46ebe8e0e0502ee4b5e0ac89707d3f965c8f453
nodus8tollens <nodus8tollens>
2023-09-20 13:21:37 +0200

    Fixed .gitignore and .env files

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
+require("dotenv").config({
+  override: true,
+  path: "development.env",
+});
+
 module.exports = {
   MONGO_DB_URL: process.env.DB_URI || "mongodb://localhost/urls",
-  LISTENING_PORT: process.env.PORT || 3000
-}
+  LISTENING_PORT: process.env.PORT || 3000,
+};
