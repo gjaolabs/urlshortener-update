c26745f6c85fd398b79399daa564987dfb561c61
nodus8tollens <nodus8tollens>
2023-10-12 16:22:43 +0200

    Fixed bug in the addPrefix utility function

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   LISTENING_PORT: process.env.PORT || 3000,
-  DB_TYPE: "postgres", //mongo or postgres
+  DB_TYPE: "mongo", //mongo or postgres
 };
--- src/util.js
+++ src/util.js
@@ -19,6 +19,8 @@ function dateToISO() {
 function addPrefix(url) {
   if (!(url.startsWith("https://") || url.startsWith("http://"))) {
     url = "https://" + url;
+  } else if (url.startsWith("http://")) {
+    url = url.replace("http://", "https://");
   }
 
   return url;
